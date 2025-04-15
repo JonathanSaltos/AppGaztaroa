463c39a2e41f4596c5f69e65a84e7ae18612a869
Jonathan S.P <jonhnspa@gmail.com>
2025-04-15 11:37:55 +0200

    Botones o Iconos

--- componentes/CampobaseComponent.js
+++ componentes/CampobaseComponent.js
@@ -1,39 +1,63 @@
 //componente de React Native que configura la navegación entre dos pantallas 
 // dentro de una aplicación móvil utilizando React Navigation. 
 import React, { Component } from 'react';// importa react y la clase componente de react,Component se usa para definir componentes de clase en React.
-import { EXCURSIONES } from '../comun/excursiones';
 import Constants from 'expo-constants'; // Importa Constants desde Expo, lo que permite acceder a constantes del entorno
 import Calendario from './CalendarioComponent';//importa el componente Calendario, para usarla en la navegacion
 import DetalleExcursion from './DetalleExcursionComponent';// importa DetalleExcursion para usarla en la navegacion
-import { Platform, View } from 'react-native';//Platform permite ejecutar código específico para iOS o Android.
+//Platform permite ejecutar código específico para iOS o Android.
 //View es un contenedor que se usa para estructurar la interfaz.
-import { NavigationContainer } from '@react-navigation/native';//es el contenedor que maneja la navegación.
+//import { NavigationContainer } from '@react-navigation/native';//es el contenedor que maneja la navegación.
 import { createNativeStackNavigator } from '@react-navigation/native-stack';//crea una pila de navegación para moverse entre pantallas.
-import { createDrawerNavigator } from '@react-navigation/drawer';
 import Home from './HomeComponent';
+import QuienesSomos from './QuienesSomosComponent';
+import Contact from './ContactoComponent';
+import { View, Platform, StyleSheet, Image, Text } from 'react-native';
+import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';//Crea navegadores tipo pila (stack).
+import { Icon } from '@rneui/themed';//Importa íconos estilizados para el menú.
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { NavigationContainer, DrawerActions } from '@react-navigation/native';//Permite usar navegación tipo drawer (menú lateral deslizable).//Contenedor obligatorio para gestionar navegación y acciones del drawer.
 const Stack = createNativeStackNavigator();//Se crea una instancia del Stack Navigator, 
 // que define la estructura de la navegación entre pantallas. Ir hacia adelante y volver hacia atras
 //Componente calendarioNavegador
-
 const Drawer = createDrawerNavigator(); // Menú lateral (desplegable).
 //Se define el componente funcional CalendarioNavegador, 
 // que maneja la navegación entre Calendario y DetalleExcursion
 // //Se usará como pantalla dentro del Drawer
 
-import QuienesSomos from './QuienesSomosComponent';
-import Contact from './ContactoComponent';
+function CustomDrawerContent(props) {//Función que personaliza el contenido del menú lateral.
+  //Renderiza el logo y nombre de la app encima de los ítems del drawer.
+  return (
+    <DrawerContentScrollView {...props}>
+      <SafeAreaView style={styles.container} forceInset={{ top: 'always', horizontal: 'never' }}>
+        <View style={styles.drawerHeader}>
+          <View style={{ flex: 1 }}>
 
-function CalendarioNavegador() {
+            <Image source={require('./imagenes/logo.png')} style={styles.drawerImage} />
+          </View>
+          <View style={{ flex: 2 }}>
+            <Text style={styles.drawerHeaderText}> Gaztaroa</Text>
+          </View>
+        </View>
+        <DrawerItemList {...props} />
+      </SafeAreaView>
+    </DrawerContentScrollView>
+  );
+}
+
+function CalendarioNavegador({ navigation }) {
   return (
     <Stack.Navigator //configura el stack de navegación.
       initialRouteName="Calendario" //define que la primera pantalla será Calendar.
-      headerMode="float"
+
       screenOptions={{ //personaliza el estilo de los encabezados de las pantallas.
         headerTintColor: '#fff',
         headerStyle: { backgroundColor: '#015afc' },
         headerTitleStyle: { color: '#fff' },
+        headerLeft: () => (<Icon name="menu" size={28} color='white'
+          onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())} />),
       }}
     >
+
       <Stack.Screen
         name="Calendario"//Agrega una pantalla llamada Calendar, que renderiza el componente Calendario
         // cuando se accede a esta pantalla
@@ -50,6 +74,8 @@ function CalendarioNavegador() {
           title: 'Detalle Excursión',//define el título en la barra de navegación.
         }}
       />
+
+
     </Stack.Navigator>
   );
 }
@@ -58,26 +84,27 @@ function CalendarioNavegador() {
 //Componente funcional HomeNavegador
 //Maneja la navegación en pila solo para la pantalla principal (Home).
 //Se integra en el Drawer como otra sección del menú.
-function HomeNavegador() {
+function HomeNavegador({ navigation }) {
   return (
-<Stack.Navigator
-    initialRouteName="Home"
-    screenOptions={{
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{
 
-     // headerMode: 'screen',
-      headerTintColor: '#fff',
-      headerStyle: { backgroundColor: '#015afc' },
-      headerTitleStyle: { color: '#fff' },
-    }}
-  > 
-  <Stack.Screen
-      name="Home"
-      component={Home}
-      options={{
-        title: 'Campo Base',
+        // headerMode: 'screen',
+        headerTintColor: '#fff',
+        headerStyle: { backgroundColor: '#015afc' },
+        headerTitleStyle: { color: '#fff' },
+        headerLeft: () => (<Icon name="menu" size={28} color='white' onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())} />),
       }}
-    />
-  </Stack.Navigator>
+    >
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{
+          title: 'Campo Base',
+        }}
+      />
+    </Stack.Navigator>
   );
 }
 
@@ -85,7 +112,7 @@ function HomeNavegador() {
 //Maneja la navegación en pila solo para la pantalla Contacto
 //Se integra en el Drawer como otra sección del menú.
 
-function ContactNavegador() {
+function ContactNavegador({ navigation }) {
 
   return (
     <Stack.Navigator
@@ -97,6 +124,7 @@ function ContactNavegador() {
         headerTintColor: '#fff',
         headerStyle: { backgroundColor: '#015afc' },
         headerTitleStyle: { color: '#fff' },
+        headerLeft: () => (<Icon name="menu" size={28} color='white' onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())} />),
       }}
     >
 
@@ -108,15 +136,15 @@ function ContactNavegador() {
         }}
       />
     </Stack.Navigator>
- 
+
   );
- 
+
 }
 
 //Componente funcional QuienesSomosNavegador
 //Maneja la navegación en pila solo para la pantalla QuienesSomos
 //Se integra en el Drawer como otra sección del menú.
-function QuienesSomosNavegador() {
+function QuienesSomosNavegador({ navigation }) {
 
   return (
     <Stack.Navigator
@@ -128,9 +156,8 @@ function QuienesSomosNavegador() {
         headerTintColor: '#fff',
         headerStyle: { backgroundColor: '#015afc' },
         headerTitleStyle: { color: '#fff' },
+        headerLeft: () => (<Icon name="menu" size={28} color='white' onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())} />),
       }}
- 
-
     >
 
       <Stack.Screen
@@ -144,17 +171,16 @@ function QuienesSomosNavegador() {
   );
 }
 
-
-
 //Componente funcional DrawerNavegador
 //Define el menú lateral (drawer) de la app.
 //Contiene dos pantallas:
-    //"Campo base" → HomeNavegador
-    //"Calendario" → CalendarioNavegador
+//"Campo base" → HomeNavegador
+//"Calendario" → CalendarioNavegador
 function DrawerNavegador() {
   return (
     <Drawer.Navigator
       initialRouteName="Campo base"
+      drawerContent={props => <CustomDrawerContent {...props} />}
       screenOptions={{
         headerShown: false,
         drawerStyle: {
@@ -162,14 +188,86 @@ function DrawerNavegador() {
         },
       }}
     >
-      <Drawer.Screen name="Campo base" component={HomeNavegador} />
-      <Drawer.Screen name="Quienes Somos" component={QuienesSomosNavegador} />
-      <Drawer.Screen name="Calendario" component={CalendarioNavegador} />
-      <Drawer.Screen name="Contacto" component={ContactNavegador} />
+      <Drawer.Screen name="Campo base" component={HomeNavegador}
+        options={{
+          drawerIcon: ({ tintColor }) => (
+            <Icon
+              name='home'
+              type='font-awesome'
+              size={24}
+              color={tintColor}
+            />
+          )
+        }}
+
+      />
+      <Drawer.Screen name="Quienes Somos" component={QuienesSomosNavegador}
+        options={{
+          drawerIcon: ({ tintColor }) => (
+            <Icon
+              name='info-circle'
+              type='font-awesome'
+              size={24}
+              color={tintColor}
+            />
+          )
+        }}
+
+      />
+      <Drawer.Screen name="Calendario" component={CalendarioNavegador}
+        options={{
+          drawerIcon: ({ tintColor }) => (
+            <Icon
+              name='calendar'
+              type='font-awesome'
+              size={24}
+              color={tintColor}
+            />
+          )
+        }}
+      />
+
+
+      <Drawer.Screen name="Contacto" component={ContactNavegador}
+        options={{
+          drawerIcon: ({ tintColor }) => (
+            <Icon
+              name='address-card'
+              type='font-awesome'
+              size={24}
+              color={tintColor}
+            />
+          )
+        }}
+
+      />
     </Drawer.Navigator>
   );
 }
-
+//Estilos del Drawer
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  drawerHeader: {
+    backgroundColor: '#015afc',
+    height: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    flexDirection: 'row'
+  },
+  drawerHeaderText: {
+    color: 'white',
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  drawerImage: {
+    margin: 10,
+    width: 80,
+    height: 60
+  }
+});
 
 //componente principal de la app.
 class Campobase extends Component {// define componente de clase llamado Campobase que extiende de react component
@@ -179,12 +277,12 @@ class Campobase extends Component {// define componente de clase llamado Campoba
       //View es un contenedor que ocupa todo el espacio disponible (flex:1).
       //ajusta la interfaz para evitar que la barra de estado se superponga en Android.
       //se renderiza Renderiza el DrawerNavegador.
-      <NavigationContainer> 
-        <View style={{flex:1, paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight }}> 
-          <DrawerNavegador /> 
-        </View> 
-          </NavigationContainer>
-      
+      <NavigationContainer>
+        <View style={{ flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight }}>
+          <DrawerNavegador />
+        </View>
+      </NavigationContainer>
+
     );
   }
 }
--- componentes/DetalleExcursionComponent.js
+++ componentes/DetalleExcursionComponent.js
@@ -1,71 +1,143 @@
 // Componente funcional, muestra una tarjeta con los detalles de la excursion si la hubiera
 // pasa la excursion a RenderExcursion
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';// importa componentes tipo Text para mostrar texto en pantalla
+import { Text, View, ScrollView, FlatList } from 'react-native';// importa componentes tipo Text para mostrar texto en pantalla
 // y View contenedor para esructurar la interfaz
-import { Card } from '@rneui/themed';// importa componente card para mostrar informacion dentro de una tarjeta
+import { Card, Icon } from '@rneui/themed';// importa componente card para mostrar informacion dentro de una tarjeta,  e iconos con estilo.
 import { EXCURSIONES } from '../comun/excursiones';//Importa una lista de excursiones desde un archivo externo.
+import { COMENTARIOS } from '../comun/comentarios';//Importa una lista de comentarios desde un archivo externo.
+import { LogBox } from 'react-native';
+import { Button } from 'react-native';
+// Ignorar advertencia de VirtualizedList dentro de ScrollView
+LogBox.ignoreLogs([
+  'VirtualizedLists should never be nested', // texto exacto o parcial del warning
+]);
+
+
+function RenderComentario(props) {//Renderiza una tarjeta con los comentarios relacionados a la excursión.
+  const comentarios = props.comentarios;
+  const renderCommentarioItem = ({ item, index }) => {
+    return (
+
+      <View key={index} style={{ margin: 10 }}>
+        <Text style={{ fontSize: 14 }}>{item.comentario}</Text>
+        <Text style={{ fontSize: 12 }}>{item.valoracion} Stars</Text>
+        <Text style={{ fontSize: 12 }}>{'- - ' + item.autor + ', ' + item.dia} </Text>
+      </View>
+    );
+  };
+
+
+  return (
+//Utiliza FlatList para mostrar los comentarios en una lista optimizada
+//Cada comentario muestra el texto, valoración y autor con fecha.
+    <Card>
+      <Card.Title>Comentarios</Card.Title>
+      <Card.Divider />
+      <FlatList
+        data={comentarios}
+        renderItem={renderCommentarioItem}
+        keyExtractor={item => item.id.toString()}
+      />
+    </Card>
+  );
+}
 
 function RenderExcursion(props) {// define la funcion RnderExcursion, componente funcional
-    //recibe el objeto props con toda la informacion de la excursion.
+  //recibe el objeto props con toda la informacion de la excursion.
 
-    const excursion = props.excursion;//extrae la excursion
-    //<Card.Title>{excursion.nombre}</Card.Title>
-        if (excursion != null) {// comprueba si el objeto excursion, esta vacio
-            return(// devuelve un componente card, el cual servira como contenedor para los datos
-                // muestra el nombre, linea divisoria, imagen, descripcion de la excursion
-                <Card>
-                <View style={{ position: 'relative' }}>
-                  <Card.Image
-                    source={require('./imagenes/40Años.png')}
-                    style={{ height: 200 }}
-                  />
-                  <View
-                    style={{
-                      position: 'absolute',
-                      top: 40,
-                      left: 0,
-                      right: 0,
-                      alignItems: 'center',
-                      padding: 35,
-                      borderRadius: 5,
-                      marginHorizontal: 10,
-                    }}
-                  >
-                    <Text
-                      style={{
-                        color: 'chocolate',
-                        fontSize: 35,
-                        fontWeight: 'bold',
-                        textAlign: 'center',
-                      }}
-                    >
-                      {excursion.nombre}
-                    </Text>
-                  </View>
-                </View>
-                <Text style={{ margin: 20 }}>{excursion.descripcion}</Text>
-              </Card>
-            );
-        }
-        else {
-            return(<View></View>);// si no esta no muestra nada, un view vacio
-        }
+  const excursion = props.excursion;//extrae la excursion
+  //<Card.Title>{excursion.nombre}</Card.Title>
+  if (excursion != null) {// comprueba si el objeto excursion, esta vacio
+    return (// devuelve un componente card, el cual servira como contenedor para los datos
+      // muestra el nombre, linea divisoria, imagen, descripcion de la excursion, un icono de corazon para mostrar como 
+      //favorita, Si ya es favorita, se muestra un heart; si no, un heart-o
+      <Card>
+        <View style={{ position: 'relative' }}>
+          <Card.Image
+            source={require('./imagenes/40Años.png')}
+            style={{ height: 200 }}
+          />
+          <View
+            style={{
+              position: 'absolute',
+              top: 40,
+              left: 0,
+              right: 0,
+              alignItems: 'center',
+              padding: 35,
+              borderRadius: 5,
+              marginHorizontal: 10,
+            }}
+          >
+            <Text
+              style={{
+                color: 'chocolate',
+                fontSize: 35,
+                fontWeight: 'bold',
+                textAlign: 'center',
+              }}
+            >
+              {excursion.nombre}
+            </Text>
+          </View>
+        </View>
+        <Text style={{ margin: 20 }}>{excursion.descripcion}</Text>
+        <Icon
+          raised
+          reverse
+          name={props.favorita ? 'heart' : 'heart-o'}
+          type='font-awesome'
+          color='#f50'
+          onPress={() => props.favorita ? console.log('La excursión ya se encuentra entre las favoritas') : props.onPress()}
+        />
+      </Card>
+    );
+  }
+  else {
+    return (<View></View>);// si no esta no muestra nada, un view vacio
+  }
 }
 
 class DetalleExcursion extends Component {// define el componente DetalleExcursion, 
-        constructor(props) {
-            super(props);
-            this.state = {
-                excursiones: EXCURSIONES //En el state, guarda la lista de excursiones (EXCURSIONES).
-            };
-        }
-      
-        render(){
-            const {excursionId} = this.props.route.params; //Extrae excursionId desde this.props.route.params
-            //  (parámetro pasado desde la navegación).
-            return(<RenderExcursion excursion={this.state.excursiones[+excursionId]} />);//Pasa la excursión a RenderExcursion, que se encargará de mostrarla.
-        }
+  constructor(props) {
+    super(props); 
+    this.state = { // inicializa el estado con excursiones,comentarios,favoritos
+      excursiones: EXCURSIONES, //En el state, guarda la lista de excursiones (EXCURSIONES).
+      comentarios: COMENTARIOS,
+      favoritos: [],
+    };
+  }
+
+  marcarFavorito(excursionId) {
+    this.setState({ favoritos: this.state.favoritos.concat(excursionId) }); //AÑADE excursion a la lista favoritos
+  }
+
+  render() {
+    const { excursionId } = this.props.route.params; //Extrae excursionId desde this.props.route.params
+    //  (parámetro pasado desde la navegación).
+    return (
+      <ScrollView>
+       {/*añade boton para volver al calendario */}
+        <View style={{ margin: 10 }}>
+          <Button
+            title="Volver Calendario"
+            onPress={() => this.props.navigation.goBack()}
+            color="#015afc"
+          />
+        </View>
+        <RenderExcursion
+          excursion={this.state.excursiones[+excursionId]}
+          favorita={this.state.favoritos.some(el => el === excursionId)}
+          onPress={() => this.marcarFavorito(excursionId)}
+        />
+        <RenderComentario
+          comentarios={this.state.comentarios.filter((comentario) => comentario.excursionId === excursionId)}
+        />
+      </ScrollView>
+    );//Pasa la excursión a RenderExcursion, que se encargará de mostrarla.
+    /// muestra los comentarios relacinados con renderComentario
+  }
 }
 
 export default DetalleExcursion;
